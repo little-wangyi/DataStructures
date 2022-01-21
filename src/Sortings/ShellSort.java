291c5f6595f1e1809ebdfaddf0cf0f4f8cb31b59
little-e <2509173866@qq.com>
2022-01-21 13:47:46 +0800

    '修改希尔排序的写法备注'

--- src/Sortings/ShellSort.java
+++ src/Sortings/ShellSort.java
@@ -3,9 +3,7 @@ package Sortings;
 import java.util.Arrays;
 
 public class ShellSort {
-    //  每次通过一个gap将原始数组分成多个数组,然后进行排序
-    //  每次循环gap/2,同上进行操作
-    //  直到gap == 1 时,为最后一次循环
+    //  每次通过一个gap将原始数组分成多个数组,然后通过后比前对数组进行排序
     public static void main(String[] args) {
         int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
         for(int gap = arr.length/2;gap>0;gap /=2) {
