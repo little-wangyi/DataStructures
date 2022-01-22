fa5f1f6ff978660f9faefc7b0de61bc7ff0e04ca
little-e <2509173866@qq.com>
2022-01-22 21:41:52 +0800

    '添加位移法实现的希尔排序'

--- src/Sortings/ShellSort.java
+++ src/Sortings/ShellSort.java
@@ -1,22 +1,57 @@
 package Sortings;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 
 public class ShellSort {
     //  每次通过一个gap将原始数组分成多个数组,然后通过后比前对数组进行排序
     public static void main(String[] args) {
-        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
-        for(int gap = arr.length/2;gap>0;gap /=2) {
+//        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        int[] arr = new int[80000];
+        for (int i = 0; i < 80000; i++) {
+            arr[i] = (int) (Math.random() * 80000);
+        }
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        String format = sdf.format(date);
+        System.out.println(format);
+        ShellSwap2(arr);
+        Date date1 = new Date();
+        String format1 = sdf.format(date1);
+        System.out.println(format1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void ShellSwap(int[] arr) {
+        //交换法
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
             for (int i = gap; i < arr.length; i++) {
-                for (int j = i-gap; j >= 0; j--) {
-                    if(arr[j] > arr[j+gap]){
+                for (int j = i - gap; j >= 0; j--) {
+                    if (arr[j] > arr[j + gap]) {
                         int temp = arr[j];
-                        arr[j] = arr[j+gap];
-                        arr[j+gap] = temp;
+                        arr[j] = arr[j + gap];
+                        arr[j + gap] = temp;
                     }
                 }
             }
         }
-        System.out.println(Arrays.toString(arr));
+    }
+
+    //位移法
+    public static void ShellSwap2(int[] arr) {
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                int temp = arr[j];
+                if(arr[j]<arr[j-gap]){
+                    while(j-gap >= 0 && temp <arr[j-gap]){
+                        arr[j] = arr[j-gap];
+                        j -= gap;
+                    }
+                    arr[j] = temp;
+                }
+            }
+        }
     }
 }
